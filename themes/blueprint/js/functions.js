22b9bdaeecc5dd2429a4acb17046873508e0ebfa
lithe <lithe@MattsMacPro-2.local>
2012-11-14 19:53:20 +1030

    Unknown updates

--- themes/blueprint/js/functions.js
+++ themes/blueprint/js/functions.js
@@ -3,12 +3,12 @@
 $(document).ready(function(){ 
    
     
-    	/*
+    	
 $("ul.navigation").superfish({ 
            //animation: {height:'show'},  
             delay:     1200             
         });
-*/
+
 
  		//if ($('#slider').length != 0) {
  			$('#slider').after('<div id="slider-pages">').cycle({ 
@@ -22,7 +22,12 @@ $("ul.navigation").superfish({
 		//}
 		
 
-         
+        $('.share').hover(function (){ $('.social-icons').stop().animate({right:'0'});
+        		$('.st_facebook_custom,.st_pinterest_custom,.st_twitter_custom,.st_email_custom').hover(function (){ $(this).find('img').css('margin-top','-80px') },function (){ $(this).find('img').css('margin-top','0px') });        	
+        	},function (){$('.social-icons').animate({right:'-125px'}); }); 
+        $('.social-icons-wrap').hover(function (){$('.social-icons').stop().animate({right:'0'}); 
+        	$('.st_facebook_custom,.st_pinterest_custom,.st_twitter_custom,.st_email_custom').hover(function (){ $(this).find('img').css('margin-top','-80px') },function (){ $(this).find('img').css('margin-top','0px') });
+        },function (){$('.social-icons').stop().animate({right:'-125px'}); }); 
                 
         
 }); 
